8f458eceb1d482e688ca4237b4e4680a279ccd0a
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-25 11:46:48 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,25 @@
-// Excercise 1
-
-function Shape(name, sides, sideLength) {
+class Shape{
+  constructor(name, sides, sideLength) {
   this.name = name;
   this.sides = sides;
   this.sideLength = sideLength;
-
-calcPerimeter = (sides, sideLength) => {
-return `${this.sides * this.sideLength}`;
-}
-
+  }
+calcPerimeter(sides, sideLength) {
+  return `${this.sides * this.sideLength}`;
+} 
 }
 
 let shapeOne = new Shape('square', 4, 5);
-
-calcPerimeter(shapeOne);
-
 let shapeTwo = new Shape('triangle', 3, 3);
 
-calcPerimeter(shapeTwo);
+class Square extends Shape{
+  constructor(name, sides, sideLength){
+  this.name = 'square';
+  this.sides = 4;
+  super(sideLength);
+  }
 
+  calcArea (sideLength) {
+    return `${sideLength*sideLength}`
+  } 
+}
